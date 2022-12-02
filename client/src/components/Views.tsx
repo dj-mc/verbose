922c2752513af02575f1e19415a2037bae722d8a
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-02 03:30:50 -0600

    Display "loading" if logged_in is null

--- client/src/components/Views.tsx
+++ client/src/components/Views.tsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Login from "./auth/Login";
 import SignUp from "./auth/SignUp";
 import NotFound from "./NotFound";
 import Protected from "./Protected";
+import { AuthContext } from "./UserContext";
 
 function Views() {
-  return (
+  const { user } = useContext(AuthContext);
+  return user.logged_in === null ? (
+    <p>Loading...</p>
+  ) : (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
