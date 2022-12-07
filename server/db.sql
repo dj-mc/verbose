87efd9ea9711fa06fdae4b63d69837163199234f
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-07 01:44:58 -0600

    Add uuid pkg, providing a unique contact_id to sql

--- server/db.sql
+++ server/db.sql
@@ -1,5 +1,6 @@
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(16) NOT NULL UNIQUE,
-    password_hash VARCHAR NOT NULL
-)
+    password_hash VARCHAR NOT NULL,
+    contact_id VARCHAR NOT NULL UNIQUE
+);
