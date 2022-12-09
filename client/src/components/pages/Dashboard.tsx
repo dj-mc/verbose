0314ef07c2f9bd6e7e8682bd90f33ed925da90bc
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-09 03:52:20 -0600

    Update UI when contacts' online status changes

--- client/src/components/pages/Dashboard.tsx
+++ client/src/components/pages/Dashboard.tsx
@@ -2,16 +2,38 @@ import { Center, Grid, GridItem, Tabs } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
 
 import Chat from "../Chat";
-import ContactsContext from "../ContactsContext";
+import ContactsContext, { IContact } from "../ContactsContext";
 import SideContactsBar from "../SideContactsBar";
 import SideNavBar from "../SideNavBar";
 import socket from "../../socket-io.js";
 import { AuthContext } from "../UserContext";
 
-function useSocket() {
+function useSocket(
+  set_contacts: React.Dispatch<React.SetStateAction<IContact[]>>
+) {
   const { set_user } = useContext(AuthContext);
   useEffect(() => {
     socket.connect();
+    // Listen for newly added contacts
+    socket.on("contacts", (contacts) => {
+      console.log('socket.on("contacts"): set_contacts:', contacts);
+      // Update UI with user's new contacts list
+      set_contacts(contacts);
+    });
+
+    // Listen for changes in online status
+    socket.on("online", (status, username) => {
+      // Update UI with contacts list online status
+      set_contacts((previous_contacts: IContact[]) => {
+        console.log('socket.on("online"): set_contacts:', previous_contacts);
+        return [...previous_contacts].map((c) => {
+          if (c.username === username) {
+            c.online = status;
+          }
+          return c;
+        });
+      });
+    });
 
     // Register callback to connect_error event
     socket.on("connect_error", () => {
@@ -22,16 +44,14 @@ function useSocket() {
     return () => {
       socket.off("connect_error");
     };
-  }, [set_user]);
+  }, [set_user, set_contacts]);
 }
 
 function Dashboard() {
-  useSocket();
-  const [contacts, set_contacts] = useState([
-    { username: "Alice", online: false },
-    { username: "Robert", online: true },
-    { username: "Seph", online: false },
-  ]);
+  const [contacts, set_contacts] = useState([]);
+
+  useSocket(set_contacts);
+
   return (
     <>
       <ContactsContext.Provider value={{ contacts, set_contacts }}>
