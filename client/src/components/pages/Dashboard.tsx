05358277b4a3389c897bcdb20513de58a5dbfd17
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-10 13:47:14 -0600

    Setup socket events for online, msg's. Fake data

--- client/src/components/pages/Dashboard.tsx
+++ client/src/components/pages/Dashboard.tsx
@@ -1,29 +1,26 @@
-import { Center, Grid, GridItem, Tabs } from "@chakra-ui/react";
+import { Grid, GridItem, Tabs } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
 
 import Chat from "../Chat";
 import ContactsContext, { IContact } from "../ContactsContext";
+import MessagesContext, { IMessage } from "../MessagesContext";
 import SideContactsBar from "../SideContactsBar";
 import SideNavBar from "../SideNavBar";
 import socket from "../../socket-io.js";
 import { AuthContext } from "../UserContext";
 
 function useSocket(
-  set_contacts: React.Dispatch<React.SetStateAction<IContact[]>>
+  set_contacts: React.Dispatch<React.SetStateAction<IContact[]>>,
+  set_messages: React.Dispatch<React.SetStateAction<IMessage[]>>
 ) {
   const { set_user } = useContext(AuthContext);
+
   useEffect(() => {
     socket.connect();
-    // Listen for newly added contacts
-    socket.on("contacts", (contacts) => {
-      console.log('socket.on("contacts"): set_contacts:', contacts);
-      // Update UI with user's new contacts list
-      set_contacts(contacts);
-    });
 
     // Listen for changes in online status
     socket.on("online", (status, username) => {
-      // Update UI with contacts list online status
+      // Update SideContactsBar UI with online status of contacts list
       set_contacts((previous_contacts: IContact[]) => {
         console.log('socket.on("online"): set_contacts:', previous_contacts);
         return [...previous_contacts].map((c) => {
@@ -35,34 +32,78 @@ function useSocket(
       });
     });
 
-    // Register callback to connect_error event
+    // Listen for newly added contacts
+    socket.on("contacts", (contacts) => {
+      console.log('socket.on("contacts"): set_contacts:', contacts);
+      // Update SideContactsBar UI with user's new contacts list
+      set_contacts(contacts);
+    });
+
+    // Listen for newly sent messages
+    socket.on("messages", (messages) => {
+      console.log('socket.on("messages"): set_messages:', messages);
+      // Update Chat's UI with new messages
+      set_messages(messages);
+    });
+
+    // Listen for connection errors
     socket.on("connect_error", () => {
       set_user({ logged_in: false });
     });
 
-    // Cleanup callback registered to connect_error event
     return () => {
+      // Cleanup events
+      socket.off("online");
+      socket.off("contacts");
+      socket.off("messages");
       socket.off("connect_error");
     };
-  }, [set_user, set_contacts]);
+  }, [set_user, set_contacts, set_messages]);
 }
 
 function Dashboard() {
-  const [contacts, set_contacts] = useState([]);
+  const [contacts, set_contacts] = useState([
+    {
+      username: "fake username 1",
+      contact_id: "fake contact id 1",
+      online: true,
+    },
+    {
+      username: "fake username 2",
+      contact_id: "fake contact id 2",
+      online: false,
+    },
+  ]);
+  const [contact_index, set_contact_index] = useState(0);
+  const [messages, set_messages] = useState([]);
 
-  useSocket(set_contacts);
+  useSocket(set_contacts, set_messages);
 
   return (
     <>
       <ContactsContext.Provider value={{ contacts, set_contacts }}>
-        <Grid templateColumns="repeat(10, 1fr)" w="100vw" h="100vh" as={Tabs}>
+        <Grid
+          as={Tabs}
+          onChange={(_) => (index: number) => {
+            console.log(_);
+            set_contact_index(index);
+            console.log(index);
+          }}
+          templateColumns="repeat(10, 1fr)"
+          w="100vw"
+          h="100vh"
+        >
           <GridItem colSpan={1}>
             <SideNavBar />
           </GridItem>
-          <GridItem colSpan={7}>
-            <Center w="100%" h="100%">
-              <Chat />
-            </Center>
+          <GridItem colSpan={7} maxW="100%" maxH="100vh">
+            <MessagesContext.Provider value={{ messages, set_messages }}>
+              {contacts.length > 0 ? (
+                <Chat contact_id={contacts[contact_index].contact_id} />
+              ) : (
+                <p style={{ marginTop: "50vh" }}>Invite someone to chat!</p>
+              )}
+            </MessagesContext.Provider>
           </GridItem>
           <GridItem colSpan={2}>
             <SideContactsBar />
