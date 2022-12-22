d29ed0885ecd2648af94496fdd6d06e1f92f382f
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-22 12:05:50 -0600

    Use tmux in dev script, add spelling

--- dev.sh
+++ dev.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-cd client; yarn run dev \
-& cd ../server; yarn run dev \
-& yarn run build:watch \
-& docker compose up -d \
-& cd ../common; yarn run build:watch
+# if [ ! -f .env ]; then
+#     export "$(cat server/.env | xargs)"
+# fi
+
+tmux \
+    new-session  "cd client || exit; yarn run dev"\; \
+    split-window "cd common || exit; yarn run build:watch"\; \
+    new-session  "cd server || exit; yarn run dev:env"\; \
+    split-window "cd server || exit; yarn run build:watch"\; \
+    new-session  "cd server || exit; docker compose up --remove-orphans"\; \
